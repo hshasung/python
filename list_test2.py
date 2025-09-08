539da46136e443a1ea7eaa29fca525e94bcb5b9e
hshasung <hshas09@naver.com>
2025-09-08 12:42:14 +0900

    Add files via upload

---
+++ list_test2.py
@@ -0,0 +1,57 @@
+
+#리스트변수 - 여러개의 값을 저장,값을 변경,[]
+'''
+score=[]
+print(score)
+print(type(score))
+
+score=[90.2, 78.6, 89.5, 67.8, 60.2, 99.5, 52.8]
+#sort() 오름차순 정렬
+score.sort()
+print(score)
+fruit=["사과","포도","파인애플","샤인머스켓","복숭아"]
+
+fruit.sort()
+print(fruit)
+
+h=[5,4,9,2,7,3]
+
+#역순 정렬
+
+h.sort()
+h.reverse()
+print(h)
+#h.sort(reverse=True)
+
+
+fruit=["사과","포도","파인애플","샤인머스켓","복숭아"]
+
+fruit.remove("파인애플")
+fruit.append("망고")
+fruit.pop(0)
+fruit.insert(2,"딸기")
+print(fruit)
+'''
+#리스트 변수 함수 append(), remove(), insert(), pop(), sort(), reverse()
+
+name_list=["김건영","김의준","김진성","남명진","박한올"]
+print(name_list)
+name_list[2]="김하성"
+print(name_list)
+name_list[4]="백지율"
+print(name_list)
+#['김건영', '김의준', '김하성', '남명진', '백지율']
+#인덱싱
+print(name_list[0:2])
+print(name_list[1:4])
+print(name_list[0:5:2])
+print(name_list[1:4:2])
+
+
+
+
+
+
+
+
+
